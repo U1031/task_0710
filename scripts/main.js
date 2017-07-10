68a3d70ff72bc6bd9a8a5da8744d58394e42899a
Yu Jin Seong <abc9816@ajou.ac.kr>
2017-07-10 17:01:54 +0900

    First Modal Commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,33 @@
-var myButton = document.querySelector('button');
+var open_modal = document.getElementById('open_modal');
+var modal_page = document.getElementById('modal_page');
+var close_btn = document.getElementById('close_btn');
+var save_btn = document.getElementById('save_btn');
+var content = document.getElementById('content');
+var close = document.getElementById('close');
+var over_lay = document.getElementById('over_lay');
 
-myButton.onclick = function(){
-    alert("wow");   
+
+open_modal.onclick = function(){
+    over_lay.style.display = "block";
+    modal_page.style.display = "block";
+}
+
+close_btn.onclick = function(){
+    modal_page.style.display = "none";
+    over_lay.style.display = "none";
+}
+
+save_btn.onclick = function(){
+    modal_page.style.display = "none";
+    over_lay.style.display = "none";
+}
+
+close.onclick = function(){
+    modal_page.style.display = "none";
+    over_lay.style.display = "none";
+}
+
+over_lay.onclick = function(){
+    modal_page.style.display = "none";
+    over_lay.style.display = "none";
 }
